7089b8b0193de35f682e63ba8c858980d6be7857
Vitaliy Akimov <vitaliy.akimiv@gmail.com>
2012-10-15 19:45:37 +0300

    fixed examples

--- examples/First.hs
+++ examples/First.hs
@@ -5,10 +5,10 @@ import Data.Dynamic.Poly
 idt = runTM (var >>>= \v -> ret (v ~> v))
 showt = runTM (var >>>= \v -> ret (showTy v ~> v ~> strTy))
 
-showC :: ProxyC (Show a) -> a -> String
-showC ProxyC = show
+showC :: Dict (Show a) -> a -> String
+showC Dict = show
 
-showTy :: Ty a a' -> Ty (ProxyC (Show a)) (ProxyC (Show a')) 
+showTy :: Ty a a' -> Ty (Dict (Show a)) (Dict (Show a')) 
 showTy t = pcTy $$ ks "Show" $$ t
 
 pcTy :: TyG Dict
@@ -19,13 +19,13 @@ strTy = kt
 
 infixr 3 ==>
 
-(==>) :: Ty i o -> Ty i' o' -> Ty (ProxyC i -> i') (ProxyC o -> o')
+(==>) :: Ty i o -> Ty i' o' -> Ty (Dict i -> i') (Dict o -> o')
 l ==> r = pcTy $$ l ~> r
 
 t1 :: Maybe String
 t1 = do
   p  <- dynApply (toDyn idt (\k -> k)) (toDynG (4 :: Int))
   p' <- dynApply (toDyn showt showC)
-        (toDyn (showTy kt) (ProxyC :: ProxyC (Show Int)))
+        (toDyn (showTy kt) (Dict :: Dict (Show Int)))
   p'' <- dynApply p' p
-  fromDynamicG p''
+  fromDynamic kt p''
